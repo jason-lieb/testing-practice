d18269f43f0df1889b3d3d77af075de00a843e5a
Jason Lieb <Jason.lieb@outlook.com>
2022-11-29 20:40:05 -0500

    initiate

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'main.js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Testing Practice'
+    })
+  ]
+}
